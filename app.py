4f593d623150e451d5d1a74e092a0b25c4502cc5
Obed Garcia Martinez <162158615+obidev777@users.noreply.github.com>
2024-03-25 23:08:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,9 @@ app = Flask(__name__)
 def file_id(id=None):
   url = f'http://127.0.0.1:7890/stream/{id}'
   return redirect(url)
+@app.route('/connect/<username>')
+def connect(username=None):
+  url = f'http://127.0.0.1:7890/connect/{username}'
+  return redirect(url)
 if __name__=='__main__':
   app.run('0.0.0.0',port=443)
